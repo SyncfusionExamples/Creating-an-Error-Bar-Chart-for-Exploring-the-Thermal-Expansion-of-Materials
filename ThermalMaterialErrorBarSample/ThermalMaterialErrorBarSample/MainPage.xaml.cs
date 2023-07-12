93888a34467d0a275768b1f9af258a3f4cb54e52
Sowndharya <sowndharya.selladurai@syncfusion.com>
2023-07-12 16:37:06 +0530

    Found the issue

---
+++ ThermalMaterialErrorBarSample/ThermalMaterialErrorBarSample/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ThermalMaterialErrorBarSample;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
---
+++ ThermalMaterialErrorBarSample/ThermalMaterialErrorBarSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ThermalMaterialErrorBarSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
